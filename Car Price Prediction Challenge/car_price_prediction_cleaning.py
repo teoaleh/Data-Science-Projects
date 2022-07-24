2f74296b810e1b29fd12387355e8adc1ea6f1620
Teo Elah <hela793@gmail.com>
2022-07-24 19:29:50 +0100

    Added the Car Price prediction Database
    I am making a small pause on this database because it needs a lot of cleaning.

---
+++ Car Price Prediction Challenge/car_price_prediction_cleaning.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+train_df = pd.read_csv('car_price_prediction.csv')
